91b93229f104b71e7169676b7b696fd59ddbc008
lavashikaa <lavashikaa@gmail.com>
2021-05-23 13:56:23 +0300

    f

---
+++ hw1.2/script/script.js
@@ -0,0 +1,12 @@
+let first = prompt('введите первое число');
+let second = prompt('введите второе число');
+alert(first - second);
+
+
+
+
+
+let firstName = prompt('введите свое имя');
+let secondName = prompt('введите свою фамилию');
+let patronymic = prompt('введите своё отчество');
+alert(firstName + ' ' + secondName + ' ' + patronymic);
